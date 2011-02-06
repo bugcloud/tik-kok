ec18367f4b788832c4de67002b2c81ece33f0e7f
bugcloud <bugcloudisco@bugcloud.com>
2011-02-06 19:03:52 +0900

    add encode

--- app/controllers/tikkok_controller.rb
+++ app/controllers/tikkok_controller.rb
@@ -9,16 +9,19 @@ class TikkokController < ApplicationController
 
   def create
     message = Mail.new(params[:message])
-    puts message.body.methods.to_yaml_style
+    puts message.body.to_yaml_style
     begin
       Tikkok.create(:title => message.subject,
                     :body => message.body.to_s.encode('utf-8'))
       if Tikkok.save
+        puts 'saved'
         render :text => "ok"
       else
+        puts 'fail to save'
         render :text => "fail to save"
       end
     rescue
+      puts "some error has occurred"
       render :text => "some error has occurred"
     end
   end

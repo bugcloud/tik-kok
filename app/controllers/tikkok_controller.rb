f69e7d4c12842df0671a5e09868638f0029d05b4
bugcloud <bugcloudisco@bugcloud.com>
2011-02-06 19:59:36 +0900

    logging

--- app/controllers/tikkok_controller.rb
+++ app/controllers/tikkok_controller.rb
@@ -9,7 +9,9 @@ class TikkokController < ApplicationController
 
   def create
     message = Mail.new(params[:message])
-    puts message.body.methods
+    message.body.each do |k,v|
+      puts "#{k} => #{v}"
+    end
     begin
       tikkok = Tikkok.new(:title => message.subject,
                           :body => message.body.to_s)

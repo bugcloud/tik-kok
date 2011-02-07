3183c317e10f56943e0d35df011895ffd893eaaf
bugcloud <bugcloudisco@bugcloud.com>
2011-02-08 00:53:44 +0900

    omg..

--- app/controllers/tikkok_controller.rb
+++ app/controllers/tikkok_controller.rb
@@ -30,9 +30,9 @@ class TikkokController < ApplicationController
   def trim_mail_data(str)
     puts "str: #{str}"
     s = str.index(" ", str.index(/charset=.* /))
-    e = str.index("--", str.index(/charset=.* /))
+    e = str.index("--", str.index("Content-Transfer-Encoding"))
     if e.nil?
-      e = str.index("--", str.index("Content-Transfer-Encoding"))
+      e = str.index("--", str.index(/charset=.* /))
     end
     (s !=nil and e != nil)? str[s..e] : str
   end

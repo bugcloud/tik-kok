bcdce28e6b9d3d324faf970404d2fbce6d3ac5b0
nagino <y.nagino@so-ra.co.jp>
2011-02-10 13:12:49 +0900

    bug

--- app/helpers/tikkok_helper.rb
+++ app/helpers/tikkok_helper.rb
@@ -1,5 +1,9 @@
 module TikkokHelper
   def mailencToUtf8(str, encoding)
-    str.encode("utf-8", encoding) unless encoding == "UTF-8" || encoding == "utf-8"
+    unless encoding == "UTF-8" || encoding == "utf-8"
+      str.encode("utf-8", encoding)
+    else
+      str
+    end
   end
 end

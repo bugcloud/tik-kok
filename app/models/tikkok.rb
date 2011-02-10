7930777a654fba9e19aaa0c4f54e6ce140c867d1
nagino <y.nagino@so-ra.co.jp>
2011-02-10 20:30:52 +0900

    edit scope

--- app/models/tikkok.rb
+++ app/models/tikkok.rb
@@ -2,7 +2,7 @@ class Tikkok < ActiveRecord::Base
 
   scope :findForDay, lambda{|day|
     y,m,d = day.year,day.month,day.day
-    where('created_at >= ?',Time.new(y,m,d)).
-      where('created_at < ?',Time.new(y,m,d+1))
+    where('created_at >= ?',Time.new(y,m,d).strftime("%Y-%m-%d 00:00:00")).
+      where('created_at < ?',Time.new(y,m,d+1).strftime("%Y-%m-%d 00:00:00"))
   }
 end

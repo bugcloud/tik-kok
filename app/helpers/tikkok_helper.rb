4b736dc620d571693b19d018c8b975ceb522b5a0
nagino <y.nagino@so-ra.co.jp>
2011-02-10 14:15:22 +0900

    fuck ガラケー

--- app/helpers/tikkok_helper.rb
+++ app/helpers/tikkok_helper.rb
@@ -1,7 +1,8 @@
 module TikkokHelper
   def mailencToUtf8(str, encoding)
     if encoding.empty?
-      return str
+      # sorry for only japanese..
+      return str.encode("utf-8", "ISO-2022-JP")
     end
     if encoding.index(/UTF\-8/i) == nil
       str.encode("utf-8", encoding)

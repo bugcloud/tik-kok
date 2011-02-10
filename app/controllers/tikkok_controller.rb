4a27ddb403ee871ac44842b68ebc24162a57da56
nagino <y.nagino@so-ra.co.jp>
2011-02-10 12:23:56 +0900

    save single body text

--- app/controllers/tikkok_controller.rb
+++ app/controllers/tikkok_controller.rb
@@ -17,10 +17,20 @@ class TikkokController < ApplicationController
 
     body = ""
     encoding = ""
+    has_plain_text = false;
+    has_html_text = false;
+
     message.body.to_s.split("\n").each {|m|
 
       if m =~ /charset=.*/
         encoding = $&.gsub("charset=","")
+      end
+
+      unless m.index("text/plain") == nil
+        has_plain_text = true
+      end
+      unless m.index("text/html") == nil
+        has_html_text = true
       end
       #debug
       puts m
@@ -28,7 +38,7 @@ class TikkokController < ApplicationController
       if m.index("Content-Type") == nil
         if m.index("Content-Transfer-Encoding") == nil
           if m.index(/\-\-[0-9a-z]*/) == nil
-            body += m
+            body += m unless has_plain_text && has_html_text
           end
         end
       end

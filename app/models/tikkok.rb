b139b7b2c18fff15d57cd3181aeb2496bdb186e6
bugcloud <bugcloudisco@bugcloud.com>
2011-02-12 19:33:18 +0900

    max 150

--- app/models/tikkok.rb
+++ app/models/tikkok.rb
@@ -4,7 +4,7 @@ class Tikkok < ActiveRecord::Base
     y,m,d = day.year,day.month,day.day
     where('created_at >= ?',Time.new(y,m,d).strftime("%Y-%m-%d 00:00:00")).
       where('created_at < ?',Time.new(y,m,d+1).strftime("%Y-%m-%d 00:00:00")).
-      order("created_at desc")
+      order("created_at desc").limit(150)
   }
 
   scope :findByDateFromAndDateTo, lambda{|from, to|
@@ -12,6 +12,6 @@ class Tikkok < ActiveRecord::Base
     ty,tm,td = to.year,to.month,to.day
     where('created_at >= ?',Time.new(fy,fm,fd).strftime("%Y-%m-%d 00:00:00")).
       where('created_at < ?',Time.new(ty,tm,td+1).strftime("%Y-%m-%d 00:00:00")).
-      order("created_at desc")
+      order("created_at desc").limit(150)
   }
 end
